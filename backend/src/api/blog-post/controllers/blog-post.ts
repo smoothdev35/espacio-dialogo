d805cad7cbd52fb233139a41b9e904d493505d4a
Hugo Priet <hugo.priet@gmail.com>
2026-08-18 16:20:32 +0200

    chore(backend): enforce required fields and default sort on content types

--- backend/src/api/blog-post/controllers/blog-post.ts
+++ backend/src/api/blog-post/controllers/blog-post.ts
@@ -1,31 +1,32 @@
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::blog-post.blog-post', ({ strapi }) => ({
-  async find(ctx) {
-    ctx.query = {
-      ...ctx.query,
-      populate: {
-        featuredImage: { populate: '*' },
-        author: { populate: { avatar: { populate: '*' } } },
-        tags: { populate: '*' },
-      },
-    }
+export default factories.createCoreController(
+  'api::blog-post.blog-post',
+  ({ strapi }) => ({
+    async find(ctx) {
+      ctx.query = {
+        ...ctx.query,
+        sort: ctx.query.sort ?? 'publishedAt:desc',
+        populate: {
+          featuredImage: { populate: '*' },
+          author: { populate: { avatar: { populate: '*' } } },
+          tags: { populate: '*' },
+        },
+      }
 
-    const sanitizedQuery = await this.sanitizeQuery(ctx)
+      const sanitizedQuery = await this.sanitizeQuery(ctx)
 
-    const entries = await strapi
-      .documents('api::blog-post.blog-post')
-      .findMany({ ...sanitizedQuery })
+      const entries = await strapi
+        .documents('api::blog-post.blog-post')
+        .findMany({ ...sanitizedQuery })
 
-    return { data: entries }
-  },
+      return { data: entries }
+    },
 
-  async findOne(ctx) {
-    const sanitizedQuery = await this.sanitizeQuery(ctx)
+    async findOne(ctx) {
+      const sanitizedQuery = await this.sanitizeQuery(ctx)
 
-    const entry = await strapi
-      .documents('api::blog-post.blog-post')
-      .findOne({
+      const entry = await strapi.documents('api::blog-post.blog-post').findOne({
         documentId: ctx.params.id,
         ...sanitizedQuery,
         populate: {
@@ -40,6 +41,7 @@ export default factories.createCoreController('api::blog-post.blog-post', ({ str
         },
       })
 
-    return { data: entry }
-  },
-}))
+      return { data: entry }
+    },
+  }),
+)
--- backend/src/api/update/controllers/update.ts
+++ backend/src/api/update/controllers/update.ts
@@ -1,23 +1,27 @@
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::update.update', ({ strapi }) => ({
-  async find(ctx) {
-    ctx.query = {
-      ...ctx.query,
-      populate: {
-        featuredImage: { populate: '*' },
-        author: { populate: { avatar: { populate: '*' } } },
-        category: { populate: '*' },
-        tags: { populate: '*' },
-      },
-    }
+export default factories.createCoreController(
+  'api::update.update',
+  ({ strapi }) => ({
+    async find(ctx) {
+      ctx.query = {
+        ...ctx.query,
+        sort: ctx.query.sort ?? 'publishedAt:desc',
+        populate: {
+          featuredImage: { populate: '*' },
+          author: { populate: { avatar: { populate: '*' } } },
+          category: { populate: '*' },
+          tags: { populate: '*' },
+        },
+      }
 
-    const sanitizedQuery = await this.sanitizeQuery(ctx)
+      const sanitizedQuery = await this.sanitizeQuery(ctx)
 
-    const entries = await strapi
-      .documents('api::update.update')
-      .findMany({ ...sanitizedQuery })
+      const entries = await strapi
+        .documents('api::update.update')
+        .findMany({ ...sanitizedQuery })
 
-    return { data: entries }
-  },
-}))
+      return { data: entries }
+    },
+  }),
+)
